9ec521de828e8d03f7f4c0e5bd1af087286249f1
mengyang <mengyang05@meituan.com>
2016-12-11 13:50:31 +0800

    Site updated: 2016-12-11 13:50:31

--- js/search.js
+++ js/search.js
@@ -1,4 +1,3 @@
-
 var searchFunc = function(path, search_id, content_id) {
     'use strict';
     $.ajax({
@@ -16,7 +15,7 @@ var searchFunc = function(path, search_id, content_id) {
             var $input = document.getElementById(search_id);
             var $resultContent = document.getElementById(content_id);
             $input.addEventListener('input', function(){
-                var str='<ul class=\"search-result-list\">';                
+                var str='<ul class=\"search-result-list\">';
                 var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                 $resultContent.innerHTML = "";
                 if (this.value.trim().length <= 0) {
@@ -51,33 +50,37 @@ var searchFunc = function(path, search_id, content_id) {
                     }
                     // show search results
                     if (isMatch) {
-                        str += "<li><a href='"+ data_url +"' class='search-result-title' target='_self'>"+ data_title;
+                        str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
                         var content = data.content.trim().replace(/<[^>]+>/g,"");
                         if (first_occur >= 0) {
-                            // cut out characters
-                            var start = first_occur - 6;
-                            var end = first_occur + 6;
+                            // cut out 100 characters
+                            var start = first_occur - 30;
+                            var outLength = 78;
                             if(start < 0){
                                 start = 0;
                             }
-                            if(start == 0){
-                                end = 10;
+                            if (start + outLength > content.length){
+                                if(content.length < outLength){
+                                    outLength = content.length - start;
+                                }else{
+                                    start = content.length - outLength;
+                                }
                             }
-                            if(end > content.length){
-                                end = content.length;
-                            }
-                            var match_content = content.substr(start, end); 
+                            var match_content = content.substr(start, outLength);
                             // highlight all keywords
                             keywords.forEach(function(keyword){
                                 var regS = new RegExp(keyword, "gi");
                                 match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
-                            })
-                            str += "<p class=\"search-result\">" + match_content +"...</p>" +"</a>";
+                            });
+
+                            str += "<p class=\"search-result\">" + match_content +"...</p>"
                         }
+                        str += "</li>";
                     }
-                })
+                });
+                str += "</ul>";
                 $resultContent.innerHTML = str;
-            })
+            });
         }
-    })
+    });
 }
